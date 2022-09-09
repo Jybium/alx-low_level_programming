f8237aacd91da66f8c61f94adb5ba130340c9782
asaolu james <asaolujtomi@gmail.com>
2022-09-08 18:17:00 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
 /**
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error
- * Return: Always 0 (Success)
+ *   followed by a new line, to the standard error
+ *   Return: Always 0 (Success)
  */
 int main(void)
 {
