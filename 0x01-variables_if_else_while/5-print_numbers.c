cdcac0780ee3d1469e824526c22bf034bc6e8e48
asaolu james <asaolujtomi@gmail.com>
2022-09-11 12:11:05 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- print all single digit number
+ * 10 statting from 0, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
