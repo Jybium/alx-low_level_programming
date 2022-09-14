7338aea0b7d10605e8cf120ad20ea9ac4dd6bbd0
asaolu james <asaolujtomi@gmail.com>
2022-09-14 06:22:31 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet -function that print alphabets in lower case
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+this is my first repository as a C language programmer
